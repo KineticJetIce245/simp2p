2b3a4233472fc5a19aaa9cff68c02d305bc5d942
KineticJetIce245 <kineticjetice245@gmail.com>
2025-11-06 11:43:09 -0500

    ui

--- src/lib/components/new-chat-subpage.svelte
+++ src/lib/components/new-chat-subpage.svelte
@@ -1,19 +1,32 @@
 <svelte:options customElement="new-chat-subpage" />
 
 <script>
+  import "$lib/theme.css";
 </script>
 
 <div class="form-container">
+  <p class="title">Conversation</p>
+  <hr class="sepline" />
+  <div class="conversation-info-form">
+    <p class="subTitle">Title</p>
+    <input id="title-input" />
+  </div>
   <p class="title">Generate</p>
+  <hr class="sepline" />
+  <div class="file-generation-form"></div>
   <p class="title">Receive</p>
+  <hr class="sepline" />
+  <div class="file-reception-form"></div>
 </div>
 
 <style>
-  @import "$lib/theme.css";
-
   .form-container {
     padding: 40px;
     font-size: 24px;
+    min-width: 400px;
+    width: 80%;
+    max-width: 600px;
+    align-self: center;
   }
 
   .title {
@@ -21,5 +34,39 @@
     font-size: 30px;
     font-weight: bold;
     color: var(--clr-light-a0);
+  }
+
+  .sepline {
+    margin: 0;
+    padding: 0;
+    border-width: 1px;
+    border-color: var(--clr-light-a10);
+  }
+
+  .conversation-info-form {
+    margin: 0;
+    padding: 10px 20px 10px 20px;
+  }
+
+  .subTitle {
+    margin: 0;
+    padding: 0;
+    font-size: 20px;
+    color: var(--clr-light-a0);
+    font-weight: bold;
+  }
+
+  #title-input {
+    padding: 7px 10px 7px 10px;
+    border-radius: 8px;
+    outline: none;
+    font-size: 14px;
+    border: 1px solid var(--clr-surface-tonal-a30);
+    background-color: var(--clr-surface-a20);
+    color: var(--clr-light-a0);
+  }
+
+  #title-input:focus {
+    border: 1px solid var(--clr-primary-a0);
   }
 </style>
